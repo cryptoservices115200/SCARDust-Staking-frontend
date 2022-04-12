1dc4aaf93dfbbf22e9c1de0e6901c09dbec98d18
foreverglorydev <happyyada.glory@gmail.com>
2022-04-12 06:13:51 -0400

    update

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -35,19 +35,19 @@ export const CONTRACTS = {
   
   [CONTRACTS_TYPE.SCARDUST_TOKEN]: {
     1: { address: '', abi: '' },
-    3: { address: "0xD4Ad95884Ff38676bD45f8ee43e0dE0CCc43f768", abi: scarDustToken.abi },
+    4: { address: "0x49685e165CcCfe6E5C828e8dd68c47c2edB18FF1", abi: scarDustToken.abi },
     56: { address: '', abi: '' },
-    97: { address: '0xD4Ad95884Ff38676bD45f8ee43e0dE0CCc43f768', abi: scarDustToken.abi },
+    97: { address: '0x49685e165CcCfe6E5C828e8dd68c47c2edB18FF1', abi: scarDustToken.abi },
   },
   [CONTRACTS_TYPE.TOKEN_DISTRIBUTOR]: {
     1: { address: '', abi: '' },
-    3: { address: "0x2A0C205CEB22Fc80b41FFa6e8A0011e4aF3aC070", abi: tokenDistributor.abi },
+    4: { address: "0xb886D50BeFE795B297D36Ee7a8f155046f3dD2b5", abi: tokenDistributor.abi },
     56: { address: '', abi: '' },
-    97: { address: '0x2A0C205CEB22Fc80b41FFa6e8A0011e4aF3aC070', abi: tokenDistributor.abi },
+    97: { address: '0xb886D50BeFE795B297D36Ee7a8f155046f3dD2b5', abi: tokenDistributor.abi },
   },
   [CONTRACTS_TYPE.FEESHARING_SYSTEM]: {
     1: { address: '', abi: '' },
-    3: { address: "0xB24c02Dc2a5D553CBCEdB7B2A6d884e37243F73d", abi: feeSharingSystem.abi },
+    4: { address: "0xB24c02Dc2a5D553CBCEdB7B2A6d884e37243F73d", abi: feeSharingSystem.abi },
     56: { address: '', abi: '' },
     97: { address: '0xB24c02Dc2a5D553CBCEdB7B2A6d884e37243F73d', abi: feeSharingSystem.abi },
   },
